eae1c3924f50b3b00792277e87213a1609d0f556
Joe <jb@joeylabs.com>
2014-04-28 12:17:59 -0400

    Uglify dependent on environment variables.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	uglify = require('gulp-uglify'),
+
+	gulpif = require('gulp-if');
+
+var env = process.env.NODE_ENV || 'development';
 
 gulp.task('jade', function (){
 	return gulp.src('src/templates/**/*.jade')
@@ -10,6 +15,7 @@ gulp.task('jade', function (){
 
 gulp.task('js', function(){
 	return gulp.src('src/js/main.js')
-		.pipe(browserify({ debug: true }))
+		.pipe(browserify({ debug: env === 'development' }))
+		.pipe(gulpif(env === 'production', uglify() ))
 		.pipe(gulp.dest('builds/development/js'));
 });
